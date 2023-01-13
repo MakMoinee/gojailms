49f62affc9dffe275ca0692338602755311a45e7
DESKTOP-LKF81KV\Mak Moinee <titus.silver.ph@gmail.com>
2023-01-13 11:46:39 +0800

    update code

--- jaildb.sql
+++ jaildb.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 30/11/2022 03:36:12
+ Date: 13/01/2023 11:46:25
 */
 
 SET NAMES utf8mb4;
@@ -69,19 +69,13 @@ CREATE TABLE `users`  (
   `password` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `userType` int NOT NULL,
   PRIMARY KEY (`userID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (11, 'mak', '$2a$14$qYx8PpZmypE5Du4Ynw0anehwk40RRJrjju4XerCOJ7IqyGnxsK/1K', 2);
-INSERT INTO `users` VALUES (12, 'ken', '$2a$14$wNnCpZhpipOQ8IQa77OSo.s.8WzEq0C03IDliI3aWBH5lglQQCtMm', 2);
-INSERT INTO `users` VALUES (13, 'sheen', '$2a$14$xkHvYqlUbhSQYRAXRcmVKO/TDgRshJOBjyOgNQEld/yqWB/D.bxqy', 2);
-INSERT INTO `users` VALUES (14, 'admin', '$2a$14$gVwmYuaMGBPH9xsRx8y5iePZQ9RcrMtBkNXn5f4kHuCRjYWyGfB4O', 1);
-INSERT INTO `users` VALUES (15, 'sheenie', '$2a$14$lg77uAmF8jrBEsCzEqT.YuyhnSBqmYOy0qb0XIqLGzKcn5fFkslkS', 2);
-INSERT INTO `users` VALUES (16, 'mak', '$2a$14$TtMEXMY54ankWb/MjN48Se1U2RnCHqar5l8LxLvFjwtCOmSW369Au', 2);
-INSERT INTO `users` VALUES (17, 'sample', '$2a$14$vTMLQJon5bs.65ZbQ3v9Te5a90q/HLxgVWSy5XIDLO2AqvDOMPs7q', 2);
 INSERT INTO `users` VALUES (18, 'admin', '$2a$14$NKT9r6172lWglVqJoKKok.h9YbaI7ouXV4FPboN3DI091UOwQLMRy', 1);
+INSERT INTO `users` VALUES (19, 'sample', '$2a$14$BJ9ad6H.sTaPtEPV3M/VPuOZNAUogV73InjSAWnPQ9J7S4kVdvEgC', 2);
 
 -- ----------------------------
 -- Table structure for visitorhistory
@@ -93,12 +87,15 @@ CREATE TABLE `visitorhistory`  (
   `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `visitedDateTime` datetime NOT NULL,
   PRIMARY KEY (`visitorHistoryID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of visitorhistory
 -- ----------------------------
 INSERT INTO `visitorhistory` VALUES (2, 9, 'she\'s wearing black vest', '2022-11-30 03:34:34');
+INSERT INTO `visitorhistory` VALUES (3, 9, 'She\'s wearing black shirt with maong pants.', '2022-11-30 12:39:27');
+INSERT INTO `visitorhistory` VALUES (4, 10, 'White shirt', '2023-01-13 08:39:48');
+INSERT INTO `visitorhistory` VALUES (5, 10, 'blue cap', '2023-01-13 09:32:28');
 
 -- ----------------------------
 -- Table structure for visitors
@@ -117,17 +114,12 @@ CREATE TABLE `visitors`  (
   `lastModifiedDate` datetime NOT NULL,
   `createdDate` datetime NOT NULL,
   PRIMARY KEY (`visitorID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of visitors
 -- ----------------------------
-INSERT INTO `visitors` VALUES (4, 11, 'mak', 'mak', 'mak', 'Door 10 Rahmann San Jose Extension, Cebu City', 'New Bataan Compostella Valley', '1998-10-13', '09365861683', '2022-08-25 23:13:00', '2022-08-25 23:13:00');
-INSERT INTO `visitors` VALUES (5, 12, 'Kennen', 'Borbon', 'Comaling', 'Purok 4A Poblacion Valencia City, Bukidnon', 'New Bataan Compostella Valley', '1998-10-13', '09269440075', '2022-08-25 23:57:38', '2022-08-25 23:57:38');
-INSERT INTO `visitors` VALUES (6, 13, 'Sheenie', 'Borbon', 'Ucab', 'Door 10 Rahmann Extension, Kamputhaw, Cebu City', 'Purok 10 Poblacion Valencia City Bukidnon', '2003-12-22', '09269440075', '2022-08-29 22:46:05', '2022-08-29 22:46:05');
-INSERT INTO `visitors` VALUES (7, 15, 'Sheenie', 'Borbon', 'x', 'Door 10 Rahmann Extension ', 'purok 10 Poblacion ', '2003-12-22', '09365861683', '2022-10-11 01:23:03', '2022-10-11 01:23:03');
-INSERT INTO `visitors` VALUES (8, 11, 'mak', 'mak', 'mak', 'mak', 'mak', '1998-10-13', '09365861683', '2022-10-13 14:46:44', '2022-10-13 14:46:44');
-INSERT INTO `visitors` VALUES (9, 17, 'sample', 'sample', 'sample', 'sample', 'sample', '2022-10-13', '09090464399', '2022-10-13 17:27:19', '2022-10-13 17:27:19');
+INSERT INTO `visitors` VALUES (10, 19, 'Sample', 'Sample', 'X', 'Poblacion Valencia City', 'New Bataan', '1998-10-13', '09365861684', '2023-01-13 07:29:16', '2023-01-13 07:29:16');
 
 -- ----------------------------
 -- View structure for vwgetallinmates
@@ -140,5 +132,11 @@ CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `vwgetallinmates` AS sele
 -- ----------------------------
 DROP VIEW IF EXISTS `vwuservisitor`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `vwuservisitor` AS select `users`.`userID` AS `userID`,`users`.`userName` AS `userName`,`visitors`.`firstName` AS `firstName`,`visitors`.`lastName` AS `lastName`,`visitors`.`middleName` AS `middleName`,`visitors`.`birthPlace` AS `birthPlace` from (`users` join `visitors` on((`users`.`userID` = `visitors`.`userID`)));
+
+-- ----------------------------
+-- View structure for vwvisithistory
+-- ----------------------------
+DROP VIEW IF EXISTS `vwvisithistory`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `vwvisithistory` AS select `visitorhistory`.`visitorHistoryID` AS `visitorHistoryID`,`visitors`.`userID` AS `userID`,`visitors`.`firstName` AS `firstName`,`visitors`.`lastName` AS `lastName`,`visitors`.`middleName` AS `middleName`,`visitorhistory`.`remarks` AS `remarks`,`visitorhistory`.`visitedDateTime` AS `visitedDateTime` from (`visitorhistory` join `visitors` on((`visitorhistory`.`visitorID` = `visitors`.`visitorID`)));
 
 SET FOREIGN_KEY_CHECKS = 1;
